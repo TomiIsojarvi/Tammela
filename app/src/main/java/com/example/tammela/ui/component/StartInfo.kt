a3c33666455bc844ad8a051a48e3aeba9d43741c
Mona Lisa <isojarvi.tomi@gmail.com>
2024-05-24 15:02:29 +0300

    Päivitä nappi lisätty alkuruutuun

--- app/src/main/java/com/example/tammela/ui/component/StartInfo.kt
+++ app/src/main/java/com/example/tammela/ui/component/StartInfo.kt
@@ -5,11 +5,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -77,6 +84,22 @@ fun StartInfo(
     val outdoorSensor = sensors.firstOrNull { it.name == "Ulkona" }
 
     Column(modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = {
+                sensorViewModel.refreshSensorData()
+                      },
+            modifier = Modifier
+                .align(alignment = Alignment.End)
+                .padding(10.dp))
+        {
+            Icon(
+                imageVector = Icons.Filled.Refresh,
+                contentDescription = "Refresh",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Päivitä")
+        }
         Row(modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
             if (indoorSensor != null && outdoorSensor != null) {
                 /*indoorSensor.let {
